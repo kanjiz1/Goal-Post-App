1a5239d84bd218b02dabfc997dafcf412a505b62
Kanjiz1 <kanjis007@gmail.com>
2018-07-03 15:56:05 +0300

    Created CreatGoalVC

---
+++ Goal Post App/Controller/CreateGoalVC.swift
@@ -0,0 +1,33 @@
+//
+//  CreateGoalVC.swift
+//  Goal Post App
+//
+//  Created by Oforkanji Odekpe on 7/3/18.
+//  Copyright © 2018 Oforkanji Odekpe. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermButton: UIButton!
+    @IBOutlet weak var longTermButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func longTermButtonWasPressed(_ sender: Any) {
+    }
+    @IBAction func shortTermButtonWasPressed(_ sender: Any) {
+    }
+    @IBAction func nextButtonWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backButtonWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
